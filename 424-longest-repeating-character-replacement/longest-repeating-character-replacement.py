5f7f2f23aa72791bcd5b94e553e7ce1d98f7d1bd
Vivek Upadhyay <viveku004@gmail.com>
2026-08-21 10:38:41 -0400

    Time: 143 ms (29.12%) | Memory: 19.6 MB (85.90%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,45 +1,27 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        '''
-        front = 0
-        back = 0
-        maxi = 0
+        slow = 0
         seen = {}
-        tempk = 0
-        size = 0
+        longest = 1
 
-        while front <= back and back <= len(s) - 1:
-            if s[back] in seen:
-                seen[s[back]] += 1
-                size += 1
-                maxi = max(maxi, size)
+        for fast in range(len(s)):
+            if s[fast] not in seen:
+                seen[s[fast]] = 1
             else:
-                seen[s[back]] = 1
-                tempk += 1
+                seen[s[fast]] +=1
 
-            back += 1
-            if tempk > k:
-                seen[s[front]] -= 1
-                front += 1
-                tempk = 0
-                size = 0
+            while (((fast - slow) + 1) - max(seen.values())) > k:
+                seen[s[slow]] -= 1
+                slow += 1
 
-        return maxi
-        '''
+            longest = max(longest, (fast - slow) + 1)
 
-        maxi = 0
-        seen = {}
-        front = 0
-        for back in range(len(s)):
-            if s[back] in seen:
-                seen[s[back]] += 1
-            else:
-                seen[s[back]] = 1
-
-            while ((back - front + 1)) - max(seen.values()) > k:
-                seen[s[front]] -= 1 
-                front += 1
-            maxi = max(maxi, (back - front + 1))
-        return maxi
+        return longest
+            
+            
 
 
+
+
+
+        
